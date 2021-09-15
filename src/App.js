9a837c7cbeec84f3ec6660eff74830c215e3e547
SumeetShelar <56186357+SumeetShelar@users.noreply.github.com>
2021-09-15 19:38:20 +0530

    signin and register connection with backend

--- src/App.js
+++ src/App.js
@@ -22,8 +22,26 @@ class App extends Component {
       imageUrl: '',
       box: {},
       route: 'signin',
-      isSignedIn: false 
+      isSignedIn: false,
+      user: {
+        id: '',
+        name: '',
+        email: '',
+        entries: 0,
+        joined: ''
+      }
     }
+  }
+
+  loadUser = (data) => {
+    this.setState({ user: {
+        id: data.id,
+        name: data.name,
+        email: data.email,
+        entries: data.entries,
+        joined: data.date
+      }
+    })
   }
 
   calculateFaceLocation = (data) => {
@@ -80,7 +98,7 @@ class App extends Component {
         :(
           route === 'signin'
           ? <Signin onRouteChange={this.onRouteChange}/>
-          : <Register onRouteChange={this.onRouteChange}/> 
+          : <Register loadUser={this.loadUser} onRouteChange={this.onRouteChange}/> 
         )
         }
         
